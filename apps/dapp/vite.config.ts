7dc4cca68f1b1c7bfc69c4f7e7c6f47b13e36419
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-08-20 20:09:32 +0900

    chore: bump dependencies

--- apps/dapp/vite.config.ts
+++ apps/dapp/vite.config.ts
@@ -5,6 +5,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [react(), svgr()],
   build: {
     rollupOptions: {

c3c178dc608914b3a10b22c8db2d00982e4bbb6c
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-08-23 14:22:28 -0300

    feat: LIGHT CLIENTOOOOOOOOOOOOOOOOOOOR

--- apps/dapp/src/lib/getApi.ts
+++ apps/dapp/src/lib/getApi.ts
@@ -3,9 +3,24 @@ import { ApiPromise, WsProvider } from "@polkadot/api";
 
 import { WS_RPC_URL } from "./settings";
 
+import {
+    ScProvider,
+    WellKnownChain,
+} from "@polkadot/rpc-provider/substrate-connect";
+import jsonParachainSpec from '../assets/gm-noleemo.json';
+
 export const getApi = async () => {
-  const wsProvider = new WsProvider(WS_RPC_URL);
-  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const gmSpec = JSON.stringify(jsonParachainSpec);
+
+  const relayProvider = new ScProvider(WellKnownChain.ksmcc3);
+  const lightProvider = new ScProvider(gmSpec, relayProvider);
+  await lightProvider.connect();
+
+  const api = await ApiPromise.create({ provider: lightProvider });
+
+  //const wsProvider = new WsProvider(WS_RPC_URL);
+  //const api = await ApiPromise.create({ provider: wsProvider });
   // need the OrmlAccountData type
   api.registerTypes(types);
   return api;

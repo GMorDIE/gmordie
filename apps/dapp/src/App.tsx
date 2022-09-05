c10c88af83a2abed2587db0c7821b8ab559e9deb
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-05 20:12:53 +0900

    merge

--- apps/dapp/src/App.tsx
+++ apps/dapp/src/App.tsx
@@ -1,10 +1,21 @@
-import { Toaster } from "react-hot-toast";
-
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { ApiProvider } from "./lib/ApiContext";
 import { GmTimeProvider } from "./lib/GmTimeContext";
 import { WalletProvider } from "./lib/WalletContext";
 import { HomePage } from "./pages/HomePage/HomePage";
+import { LeaderboardPage } from "./pages/Leaderboard/LeaderboardPage";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const Pages = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/leaderboard" element={<LeaderboardPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 function App() {
   return (
@@ -12,7 +23,7 @@ function App() {
       <ApiProvider>
         <WalletProvider>
           <GmTimeProvider>
-            <HomePage />
+            <Pages />
           </GmTimeProvider>
         </WalletProvider>
       </ApiProvider>
---
+++ apps/dapp/src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -0,0 +1,5 @@
+import { Layout } from "../../components/Layout"
+
+export const LeaderboardPage = () => {
+    return <Layout>Leaderboard</Layout>
+}

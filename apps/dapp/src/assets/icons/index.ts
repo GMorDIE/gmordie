9523f2686f7a5ca3cb906b57b3f13d976089b933
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-09 00:19:56 +0900

    fix: send button rework

---
+++ apps/dapp/src/assets/icons/index.ts
@@ -0,0 +1,1 @@
+export { ReactComponent as IconSend } from "./send.svg";
--- apps/dapp/src/features/send/SendButton.tsx
+++ apps/dapp/src/features/send/SendButton.tsx
@@ -1,8 +1,8 @@
+import { IconSend } from "../../assets/icons";
 import { Button } from "../../components/Button";
 import { useWallet } from "../../lib/WalletContext";
 import { SendModal } from "./SendModal";
 import { SendModalProvider, useSendModal } from "./context";
-import { PaperAirplaneIcon } from "@heroicons/react/solid";
 
 const SendButtonInner = () => {
   const { open } = useSendModal();
@@ -11,8 +11,13 @@ const SendButtonInner = () => {
   if (!account) return null;
 
   return (
-    <Button onClick={open} title="Send GM or GN">
-      <PaperAirplaneIcon className="w-6 h-6 rotate-90" />
+    <Button
+      onClick={open}
+      title="Send GM or GN"
+      className="flex gap-2 text-sm items-center border !py-1"
+    >
+      <span>Send it!</span>
+      <IconSend className="h-4 w-4" />
     </Button>
   );
 };

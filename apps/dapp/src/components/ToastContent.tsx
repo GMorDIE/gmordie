36a224c0040d13aeee000c72e1d2ef02a78e27ec
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-15 20:10:38 +0900

    feat: enhanced tx follow-up toasts

--- apps/dapp/src/components/ToastContent.tsx
+++ apps/dapp/src/components/ToastContent.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import { ReactNode } from "react";
 import toast, { Toast } from "react-hot-toast";
 
-type ToastType = "success" | "error" | "loading";
+type ToastType = "success" | "error" | "loading" | "pending";
 
 const TypeIcon = ({ type }: { type?: ToastType }) => {
   switch (type) {
@@ -21,6 +21,7 @@ const TypeIcon = ({ type }: { type?: ToastType }) => {
         </div>
       );
     case "loading":
+    case "pending":
       return (
         <div className="flex flex-col justify-center items-center w-10 h-10">
           <Spinner className="h-8 w-8 text-white" />
@@ -51,19 +52,20 @@ export const ToastContent = ({
     <div
       className={clsx(
         t.visible ? "animate-enter" : "animate-leave",
-        `w-80 p-2 px-2 gap-2 items-center max-w-md shadow-2xl rounded-lg pointer-events-auto flex ring-1 ring-white ring-opacity-10`,
-        type === "error" && "bg-red-600",
-        type === "success" && "bg-green-600",
-        type === "loading" && "bg-salmon-800"
+        `w-80 p-2 px-2 gap-2 items-center max-w-md rounded-lg pointer-events-auto flex ring-1 ring-white ring-opacity-10 transition-colors duration-500 shadow-2xl `,
+        type === "error" && "bg-red-700",
+        type === "success" && "bg-green-700",
+        type === "pending" && "bg-teal-700",
+        type === "loading" && "bg-sky-700"
       )}
     >
       <div>
         <TypeIcon type={type} />
       </div>
       <div className="grow flex flex-col justify-center ">
-        <div className="text-sm font-bold text-white">{title}</div>
+        <div className="text-base font-bold text-white">{title}</div>
         {description && (
-          <div className="text-xs text-zinc-100">{description}</div>
+          <div className="text-sm text-zinc-100 font-medium">{description}</div>
         )}
       </div>
       <button
--- apps/dapp/src/lib/getSignAndSendCallback.ts
+++ apps/dapp/src/lib/getSignAndSendCallback.ts
@@ -5,11 +5,24 @@ export const getSignAndSendCallback = () => {
   let id = "";
   let finished = false;
 
-  return async (result: ISubmittableResult) => {
+  return async ({ events, status }: ISubmittableResult) => {
     // prevents error to be displayed multiple times
     if (finished) return;
 
-    if (!id) {
+    if (status.isInvalid) {
+      showToast(
+        {
+          title: "Transaction is invalid",
+          description: "༼☯﹏☯༽ NGMI...",
+          type: "error",
+        },
+        {
+          id,
+          duration: Infinity,
+        }
+      );
+      finished = true;
+    } else if (status.isReady) {
       id = showToast(
         {
           title: "Submitting transaction",
@@ -20,33 +33,11 @@ export const getSignAndSendCallback = () => {
           duration: Infinity,
         }
       );
-    }
-
-    const ngmi = result.findRecord("system", "ExtrinsicFailed");
-
-    if (ngmi || result.isError) {
-      console.error("Transaction failed", {
-        dispatchError: result.dispatchError?.toHuman(true),
-        internalError: result.dispatchError?.toHuman(true),
-        failedExtrinsic: ngmi?.toHuman(true),
-      });
+    } else if (status.isBroadcast) {
       showToast(
         {
-          title: "Transaction failed",
-          description: "༼☯﹏☯༽ NGMI...",
-          type: "error",
-        },
-        {
-          id,
-          duration: Infinity,
-        }
-      );
-      finished = true;
-    } else if (result.isInBlock) {
-      showToast(
-        {
-          title: "Submitted",
-          description: "Waiting for confirmation...",
+          title: "Submitting transaction",
+          description: "Waiting to be included in a block...",
           type: "loading",
         },
         {
@@ -54,19 +45,54 @@ export const getSignAndSendCallback = () => {
           duration: Infinity,
         }
       );
-    } else if (result.isCompleted || result.isFinalized) {
+    } else if (status.isInBlock) {
       showToast(
         {
-          title: "Success",
-          description: "Nailed it! 🤘",
-          type: "success",
+          title: "Transaction accepted",
+          description: "Waiting for confirmation...",
+          type: "pending",
         },
         {
           id,
-          duration: 2000,
+          duration: Infinity,
         }
       );
-      finished = true;
+    } else if (status.isFinalized) {
+      const success = events.find(
+        ({ event }) => event.method === "ExtrinsicSuccess"
+      );
+      const failed = events.find(
+        ({ event }) => event.method === "ExtrinsicFailed"
+      );
+
+      if (success) {
+        showToast(
+          {
+            title: "Success",
+            description: "Nailed it! 🤘",
+            type: "success",
+          },
+          {
+            id,
+            duration: 3000,
+          }
+        );
+        finished = true;
+      } else if (failed) {
+        showToast(
+          {
+            title: "Transaction failed",
+            description: "༼☯﹏☯༽ NGMI...",
+            type: "error",
+          },
+          {
+            id,
+            duration: Infinity,
+          }
+        );
+        finished = true;
+        console.error(failed.toHuman(true));
+      } else throw new Error("unknown transaction result");
     }
   };
 };

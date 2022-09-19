c06ca5cf17e4d17032bac49cb8f3e5ddb8f36a05
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-19 17:09:24 +0900

    fix: FREN burn

--- apps/dapp/src/lib/tokensToPlanck.ts
+++ apps/dapp/src/lib/tokensToPlanck.ts
@@ -1,4 +1,4 @@
-import { BN, BN_ZERO } from "@polkadot/util";
+import { BN, BN_TEN } from "@polkadot/util";
 
 type BNish = string | number | BN | Uint8Array | number[] | Buffer;
 
@@ -6,8 +6,8 @@ type BNish = string | number | BN | Uint8Array | number[] | Buffer;
 export const tokensToPlanck = (tokens: BNish, decimals: number) => {
   const tokenAmount = new BN(tokens);
   const tokenDecimals = new BN(decimals);
-  const base = BN_ZERO;
+  const base = BN_TEN;
   const planck = tokenAmount.mul(base.pow(tokenDecimals));
-
+  console.log({ tokens: tokens.toString(), planck: planck.toString() });
   return planck;
 };

97ce54db22e9a6f5872223fca691aa9be884aa24
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-09-19 23:01:02 +0900

    fix: id check - msg if no identity set

--- apps/dapp/src/pages/IdentityVerificationPage/IdentityDisplay.tsx
+++ apps/dapp/src/pages/IdentityVerificationPage/IdentityDisplay.tsx
@@ -1,6 +1,8 @@
 import { Spinner } from "../../components/Spinner";
 import { IdentityJudgements } from "./IdentityJudgements";
 import { useIdentityVerification } from "./useIdentityVerification";
+import { ShieldCheckIcon } from "@heroicons/react/solid";
+import clsx from "clsx";
 import { useMemo } from "react";
 
 type IdentityDisplayProps = {
@@ -14,7 +16,7 @@ export const IdentityDisplay = ({ address }: IdentityDisplayProps) => {
     () =>
       data?.registration
         ? JSON.stringify(data.registration.toHuman(true), undefined, 3)
-        : "",
+        : null,
     [data]
   );
 
@@ -27,16 +29,27 @@ export const IdentityDisplay = ({ address }: IdentityDisplayProps) => {
       </div>
     );
 
-  if (!data) return null;
+  if (isLoading) return null;
 
   return (
     <div className="flex flex-col pb-8 gap-2">
-      <IdentityJudgements
-        address={address}
-        registration={data.registration}
-        judges={data.judges}
-      />
-      <pre className="bg-zinc-800 rounded p-2">{stringified}</pre>
+      {data ? (
+        <>
+          <IdentityJudgements
+            address={address}
+            registration={data.registration}
+            judges={data.judges}
+          />
+          <pre className="bg-zinc-800 rounded p-2">{stringified}</pre>
+        </>
+      ) : (
+        <div className="text-white font-bold">
+          <ShieldCheckIcon
+            className={clsx("inline w-6 h-6 mb-1 text-red-500")}
+          />{" "}
+          Identity has not been set
+        </div>
+      )}
     </div>
   );
 };

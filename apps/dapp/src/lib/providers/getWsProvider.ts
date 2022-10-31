176ade7252b655540f4015bb4638a49881e80eed
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-10-31 19:16:25 +0900

    chore: bump deps

--- apps/dapp/src/lib/providers/getWsProvider.ts
+++ apps/dapp/src/lib/providers/getWsProvider.ts
@@ -4,22 +4,6 @@ import { WsProvider } from "@polkadot/api";
 const TIMEOUT = 10_000;
 
 export const getWsProvider = async () => {
-  // Promise.any returns the fastest WsProvider to be ready
-  // This should spread the load on all available nodes
-  const providers = WS_RPC_URLS.map((url) => new WsProvider(url, TIMEOUT));
-  const provider = await Promise.any(providers.map((p) => p.isReady));
-
-  // disconnect other ones
-  providers.forEach(async (p, i) => {
-    try {
-      if (p !== provider) {
-        await p.disconnect();
-      }
-    } catch (err) {
-      console.log(`failed to disconnect from ${WS_RPC_URLS[i]}`);
-      console.error(err);
-    }
-  });
-
-  return provider;
+  // use built-in rpc switching
+  return new WsProvider(WS_RPC_URLS, TIMEOUT);
 };
